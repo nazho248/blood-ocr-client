fd3e48afe22ac927cfc17422ca25ee6c673a84ab
nazho248 <jairandres24@hotmail.es>
2025-05-07 09:04:05 -0500

    add build in vite config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import preact from '@preact/preset-vite';
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,4 +13,7 @@ export default defineConfig({
 			},
 		}),
 	],
+	build: {
+		outDir: 'build',
+	},
 });
